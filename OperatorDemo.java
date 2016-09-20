0e7518fe9503926eb7d9e6cdd27c4550ae60872a
Arpita30 <arpita.salecha.jain@gmail.com>
2016-09-20 12:42:51 +0530

    Add files via upload

---
+++ OperatorDemo.java
@@ -0,0 +1,19 @@
+
+public class OperatorDemo {
+
+	
+	public static void main(String[] args) {
+		int a=20;
+		System.out.println("value of a is: "+a);
+		int b= a-- - --a;
+		System.out.println("value of b is: "+b);
+		int c=a--;
+		System.out.println("value of c is: "+c);
+		int d=a>>2;
+	    System.out.println("value of d is: "+d);;
+		int e=a&b;
+		System.out.println("value of e is: "+e);
+
+	}
+
+}
